ad75134a72a358da340aed0d24f169d9f771b7d8
Shubham Dokania <skd.1810@gmail.com>
2015-07-25 19:58:18 +0530

    Time for my special commits

---
+++ bytes/admin.py
@@ -0,0 +1,34 @@
+'''
+class ModelBAdmin(admin.ModelAdmin):
+
+    def modelA_Codes(self, inst):
+        return ','.join([b.code for b in inst.modela_set.all()])
+
+    list_display = ('name', 'modelA_Codes')
+
+class HotelAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'rating', 'logo', 'rooms', 'hotel_id']
+
+
+class AppliancesAdmin(admin.ModelAdmin):
+
+    list_display = ['room_number', 'appliance_id', 'on', 'name']
+
+
+class RoomAdmin(admin.ModelAdmin):
+
+    list_display = ['hotel_id', 'user', 'room_server', 'room_number', 'type_id', 'type_name', 'appliances']
+
+
+
+class HotelRoomsAdmin(admin.ModelAdmin):
+
+    list_display = ['type_id', 'type_name', 'total', 'available', 'rate', 'beds']
+
+
+admin.site.register(Hotel, HotelAdmin)
+admin.site.register(HotelRooms, HotelRoomsAdmin)
+admin.site.register(Appliances, AppliancesAdmin)
+admin.site.register(Room, RoomAdmin)
+'''
--- bytes_server/settings.py
+++ bytes_server/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = PACKAGE_ROOT
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bytes',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +84,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [
+    os.path.join(PACKAGE_ROOT, "templates"),
+]
